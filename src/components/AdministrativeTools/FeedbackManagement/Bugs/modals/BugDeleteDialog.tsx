b2b9b748b60c2787e3246da152efca65b6466b79
mohamedaminebennacef <aminebennacef098@gmail.com>
2025-03-28 00:24:50 +0100

    feat(bug) : initialize bug data-table

---
+++ src/components/AdministrativeTools/FeedbackManagement/Bugs/modals/BugDeleteDialog.tsx
@@ -0,0 +1,62 @@
+import { useDialog } from "@/components/Common/Dialogs";
+import { Spinner } from "@/components/Common/Spinner";
+import { Button } from "@/components/ui/button";
+
+interface BugDeleteDialogProps {
+  bugMessage?: string;
+  deleteBug?: () => void;
+  isDeletionPending?: boolean;
+  resetBug?: () => void;
+}
+
+export const useBugDeleteDialog = ({
+  bugMessage,
+  deleteBug,
+  isDeletionPending,
+  resetBug,
+}: BugDeleteDialogProps) => {
+  const {
+    DialogFragment: deleteBugDialog,
+    openDialog: openDeleteBugDialog,
+    closeDialog: closeDeleteBugDialog,
+  } = useDialog({
+    title: (
+      <div className="leading-normal">
+        Delete Bug <span className="font-light">{bugMessage}</span> ?
+      </div>
+    ),
+    description: "This action is permanent and cannot be undone.",
+    children: (
+      <div>
+        <div className="flex gap-2 justify-end">
+          <Button
+            onClick={() => {
+              deleteBug?.();
+              closeDeleteBugDialog();
+            }}
+          >
+            Confirm
+            <Spinner show={isDeletionPending} />
+          </Button>
+          <Button
+            variant={"secondary"}
+            onClick={() => {
+              resetBug?.();
+              closeDeleteBugDialog();
+            }}
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    ),
+    className: "w-[500px]",
+    onToggle: resetBug,
+  });
+
+  return {
+    deleteBugDialog,
+    openDeleteBugDialog,
+    closeDeleteBugDialog,
+  };
+};
---
+++ src/hooks/stores/useBugManager.tsx
@@ -0,0 +1,61 @@
+import { Bug, BugCategory } from "@/types/bug";
+import { create } from "zustand";
+
+interface BugManagerData {
+  id?: number;
+  title?: string;
+  description?: string;
+  category?: BugCategory;
+}
+
+interface BugManager extends BugManagerData {
+  set: (name: keyof BugManagerData, value: any) => void;
+  reset: () => void;
+  getBug: () => Partial<Bug>;
+  setBug: (data: Partial<Bug>) => void;
+}
+
+const initialState: BugManagerData = {
+  id: undefined,
+  title: "",
+  description: "",
+  category: undefined,
+};
+
+export const useBugManager = create<BugManager>((set, get) => ({
+  ...initialState,
+
+  set: (name: keyof BugManager, value: any) => {
+    set((state) => ({
+      ...state,
+      [name]: value,
+    }));
+  },
+
+  reset: () => {
+    set({ ...initialState });
+  },
+
+  getBug: () => {
+    const data = get();
+    return {
+      id: data.id,
+      title: data.title,
+      description: data.description,
+      category: data.category,
+    };
+  },
+
+  setBug: (data: Partial<Bug>) => {
+    set(
+      (state) =>
+        ({
+          ...state,
+          id: data.id,
+          title: data.title,
+          description: data.description,
+          category: data.category,
+        } as Partial<BugManager>)
+    );
+  },
+}));
