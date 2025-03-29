2c7b6da2c91affafc5e71c5f8fa0779005f190b5
mohamedaminebennacef <aminebennacef098@gmail.com>
2025-03-29 12:28:22 +0100

    feat(bug data-table) : add checkbox functionality to select individual rows or all rows in the data table

--- src/components/AdministrativeTools/FeedbackManagement/Bugs/data-table/columns.tsx
+++ src/components/AdministrativeTools/FeedbackManagement/Bugs/data-table/columns.tsx
@@ -1,10 +1,40 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "./data-table-column-header";
 import { DataTableRowActions } from "./data-table-row-actions";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Bug } from "@/types/bug";
 
 export const getBugColumns = (): ColumnDef<Bug>[] => {
   return [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <div className="w-10 flex justify-center">
+          <Checkbox
+            className="w-4 h-4"
+            checked={
+              table.getIsAllPageRowsSelected() ||
+              (table.getIsSomePageRowsSelected() && "indeterminate")
+            }
+            onCheckedChange={(value) =>
+              table.toggleAllPageRowsSelected(!!value)
+            }
+            aria-label="Select all"
+          />
+        </div>
+      ),
+      cell: ({ row }) => (
+        <div className="w-10 flex justify-center">
+          <Checkbox
+            checked={row.getIsSelected()}
+            onCheckedChange={(value) => row.toggleSelected(!!value)}
+            aria-label="Select row"
+          />
+        </div>
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
     {
       accessorKey: "title",
       header: ({ column }) => (
--- src/components/AdministrativeTools/FeedbackManagement/Bugs/data-table/data-table-pagination.tsx
+++ src/components/AdministrativeTools/FeedbackManagement/Bugs/data-table/data-table-pagination.tsx
@@ -24,67 +24,73 @@ export function DataTablePagination<TData>({
   const { page, totalPageCount, setPage, size, setSize } = useBugActions();
 
   return (
-    <div className="flex items-center justify-between pt-4">
-      <div className="flex items-center space-x-2 ">
-        <Select
-          value={size.toString()}
-          onValueChange={(value) => {
-            setPage(1);
-            setSize(parseInt(value));
-          }}
-        >
-          <SelectTrigger className="h-8 w-[100px] -mt-1">
-            <SelectValue placeholder={table.getState().pagination.pageSize} />
-          </SelectTrigger>
-          <SelectContent side="bottom" align="center">
-            {[5, 10, 20].map((pageSize) => (
-              <SelectItem key={pageSize} value={`${pageSize}`}>
-                {pageSize}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-        <p className="text-sm font-medium">Row per page</p>
+    <>
+      <div className="$text-sm text-muted-foreground">
+        {table.getFilteredSelectedRowModel().rows.length} of{" "}
+        {table.getFilteredRowModel().rows.length} row(s) selected.
       </div>
-      <div className="flex items-center space-x-6 lg:space-x-8">
-        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
-          Page {page} of {totalPageCount}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-2 ">
+          <Select
+            value={size.toString()}
+            onValueChange={(value) => {
+              setPage(1);
+              setSize(parseInt(value));
+            }}
+          >
+            <SelectTrigger className="h-8 w-[100px] -mt-1">
+              <SelectValue placeholder={table.getState().pagination.pageSize} />
+            </SelectTrigger>
+            <SelectContent side="bottom" align="center">
+              {[5, 10, 20].map((pageSize) => (
+                <SelectItem key={pageSize} value={`${pageSize}`}>
+                  {pageSize}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <p className="text-sm font-medium">Row per page</p>
         </div>
-        <div className="flex items-center space-x-2">
-          <Button
-            variant="outline"
-            className="hidden h-8 w-8 p-0 lg:flex"
-            onClick={() => setPage(1)}
-            disabled={page == 1}
-          >
-            <DoubleArrowLeftIcon className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="outline"
-            className="h-8 w-8 p-0"
-            onClick={() => setPage(page - 1)}
-            disabled={page <= 1}
-          >
-            <ChevronLeftIcon className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="outline"
-            className="h-8 w-8 p-0"
-            onClick={() => setPage(page + 1)}
-            disabled={page >= totalPageCount}
-          >
-            <ChevronRightIcon className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="outline"
-            className="hidden h-8 w-8 p-0 lg:flex"
-            onClick={() => setPage(totalPageCount)}
-            disabled={page == totalPageCount}
-          >
-            <DoubleArrowRightIcon className="h-4 w-4" />
-          </Button>
+        <div className="flex items-center space-x-6 lg:space-x-8">
+          <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+            Page {page} of {totalPageCount}
+          </div>
+          <div className="flex items-center space-x-2">
+            <Button
+              variant="outline"
+              className="hidden h-8 w-8 p-0 lg:flex"
+              onClick={() => setPage(1)}
+              disabled={page == 1}
+            >
+              <DoubleArrowLeftIcon className="h-4 w-4" />
+            </Button>
+            <Button
+              variant="outline"
+              className="h-8 w-8 p-0"
+              onClick={() => setPage(page - 1)}
+              disabled={page <= 1}
+            >
+              <ChevronLeftIcon className="h-4 w-4" />
+            </Button>
+            <Button
+              variant="outline"
+              className="h-8 w-8 p-0"
+              onClick={() => setPage(page + 1)}
+              disabled={page >= totalPageCount}
+            >
+              <ChevronRightIcon className="h-4 w-4" />
+            </Button>
+            <Button
+              variant="outline"
+              className="hidden h-8 w-8 p-0 lg:flex"
+              onClick={() => setPage(totalPageCount)}
+              disabled={page == totalPageCount}
+            >
+              <DoubleArrowRightIcon className="h-4 w-4" />
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
