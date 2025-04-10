6fe72a9b6b0d5fc8f46def819485bf52ecb3a7de
mohamedaminebennacef <aminebennacef098@gmail.com>
2025-04-10 22:29:08 +0100

    chore(SystemReports) : remove FeedbackManagement layout component

--- src/pages/system-reports/bugs.tsx
+++ src/pages/system-reports/bugs.tsx
@@ -1,10 +1,7 @@
-import FeedbacksManagement from   "@/components/AdministrativeTools/SystemReports/FeedbackManagement";
 import Bugs from "@/components/AdministrativeTools/SystemReports/Bugs/Bugs";
 
 export default function page() {
   return (
-    <FeedbacksManagement>
       <Bugs />
-    </FeedbacksManagement>
   );
 }
--- src/pages/system-reports/feedbacks.tsx
+++ src/pages/system-reports/feedbacks.tsx
@@ -1,10 +1,7 @@
-import FeedbacksManagement from "@/components/AdministrativeTools/SystemReports/FeedbackManagement";
 import Feedbacks from "@/components/AdministrativeTools/SystemReports/Feedbacks/Feedbacks";
 
 export default function page() {
   return (
-    <FeedbacksManagement>
       <Feedbacks />
-    </FeedbacksManagement>
   );
 }
