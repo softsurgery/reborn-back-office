f77530a56617e31087ce140c9c47b60e8f4ba543
mohamedaminebennacef <aminebennacef098@gmail.com>
2025-04-10 21:48:11 +0100

    feat(translation) : add i18n translations

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,77 +1,31 @@
 import React from "react";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { PanelLeft, Search, User } from "lucide-react";
-import { Input } from "@/components/ui/input";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
-import { ResponsiveSidebar } from "./ResponsiveSidebar";
-import { MenuItem } from "./StaticMenu";
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/router";
+import { useBreadcrumb } from "../../context/BreadcrumbContext";
+import { SidebarTrigger } from "../ui/sidebar";
+import { BreadcrumbCommon } from "../Common/Breadcrumb";
 import { ModeToggle } from "../Common/ModeToggle";
-import { useBreadcrumb } from "@/context/BreadcrumbContext";
-import { Breadcrumb } from "../Common/Breadcrumb";
+import { LanguageSwitcher } from "../Common/LanguageSwitcher";
 
 interface HeaderProps {
   className?: string;
-  items?: MenuItem[];
 }
 
-export const Header: React.FC<HeaderProps> = ({ className, items }) => {
-  const router = useRouter();
+export const Header = ({ className }: HeaderProps) => {
   const { routes } = useBreadcrumb();
 
   return (
-    <header className="sticky top-0 z-30 flex min-h-14 items-center gap-4 border-b bg-background px-4">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button size="icon" variant="outline" className="sm:hidden">
-            <PanelLeft className="h-5 w-5" />
-            <span className="sr-only">Toggle Menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left" className={cn("sm:max-w-xs", className)}>
-          <ResponsiveSidebar items={items} />
-        </SheetContent>
-      </Sheet>
-      <Breadcrumb routes={routes} />
-      <div className="relative ml-auto flex-1 md:grow-0">
-        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-        <Input
-          type="search"
-          placeholder="Search..."
-          className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-        />
+    <header
+      className={cn(
+        "flex h-14 items-center gap-2 border-b px-4 lg:h-[60px] lg:px-6 w-full",
+        className
+      )}
+    >
+      <SidebarTrigger />
+      <BreadcrumbCommon hierarchy={routes} />
+      <div className="flex justify-center items-center gap-4 ml-auto">
+        <LanguageSwitcher />
+        <ModeToggle />
       </div>
-      <ModeToggle />
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant="outline"
-            size="icon"
-            className="overflow-hidden rounded-full"
-          >
-            <User />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Settings</DropdownMenuItem>
-          <DropdownMenuItem>Support</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => router.push("/sign-out")}>
-            Logout
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
     </header>
   );
 };
