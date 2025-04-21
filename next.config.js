1b614f23aed0295d7c2f998f83cfb3cbcea730ce
zc-houssem <houssemeddine.chibouni@zedneycreative.com>
2025-04-21 16:51:26 +0100

    fix(app-version): repair app version next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const { version } = require("./package.json");
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_APP_VERSION: version,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
