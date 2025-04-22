1fb163e0979fcc60bfd596d84ef4e5359c91cedd
zc-houssem <houssemeddine.chibouni@zedneycreative.com>
2025-04-22 17:10:20 +0100

    chore: remove protection from pending approval

--- src/pages/auth/pending.tsx
+++ src/pages/auth/pending.tsx
@@ -1,34 +1,5 @@
 import PendingApproval from "@/components/Common/pages/PendingApproval";
-import { GetServerSidePropsContext } from "next";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]";
 
 export default function Page() {
   return <PendingApproval />;
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context.req, context.res, authOptions);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/auth",
-        permanent: false,
-      },
-    };
-  }
-
-  if (session.user?.isApproved) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {},
-  };
 }
--- src/pages/auth/still-pending.tsx
+++ src/pages/auth/still-pending.tsx
@@ -1,34 +1,5 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { GetServerSidePropsContext } from "next";
 import StillPending from "@/components/Common/pages/PendingApproval2";
 
 export default function StillPendingPage() {
   return <StillPending />;
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context.req, context.res, authOptions);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/auth",
-        permanent: false,
-      },
-    };
-  }
-
-  if (session.user?.isApproved) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {},
-  };
 }
