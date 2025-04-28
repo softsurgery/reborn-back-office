e392fb09f7e54aaa811d675b6b813f8a4de37fed
zc-houssem <houssemeddine.chibouni@zedneycreative.com>
2025-04-28 21:17:05 +0100

    fix(Region): use MobileUser type instead of old AppUser with Region

--- src/types/content/region.ts
+++ src/types/content/region.ts
@@ -1,8 +1,8 @@
-import { AppUser } from "../user-management";
+import { MobileUser } from "../user-management";
 import { DatabaseEntity } from "../utils/database-entity";
 
 export interface Region extends DatabaseEntity {
   id: number;
   label: string;
-  AppUser?: AppUser[];
+  AppUser?: MobileUser[];
 }
