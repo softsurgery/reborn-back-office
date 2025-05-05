3e403be99cf5b5dc80910f9bd14350934747b868
zc-houssem <houssemeddine.chibouni@zedneycreative.com>
2025-05-05 01:01:19 +0100

    fix(user-nav): add fallback values for  identify user utilities

--- src/lib/users-management/utils/identify-user.util.ts
+++ src/lib/users-management/utils/identify-user.util.ts
@@ -6,7 +6,7 @@ export const identifyUser = (user: User | null) => {
     ? `${user?.firstName?.charAt(0).toUpperCase() + user?.firstName.slice(1)} ${
         user?.lastName?.charAt(0).toUpperCase() + user?.lastName.slice(1)
       }`
-    : user?.username;
+    : user?.username || "unknown";
 };
 
 export const identifyUserAvatar = (user: User | null) => {
@@ -15,5 +15,5 @@ export const identifyUserAvatar = (user: User | null) => {
     ? `${user?.firstName?.charAt(0).toUpperCase()}${user?.lastName
         ?.charAt(0)
         .toUpperCase()}`
-    : user?.username?.charAt(0).toUpperCase();
+    : user?.username?.charAt(0).toUpperCase() || "?";
 };
