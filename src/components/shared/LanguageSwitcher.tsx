527e765709fc4eb992277c36311c3dc3c8a38b78
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2025-08-04 15:16:19 +0100

    fix(language-switcher): repair fallback on loading errors

--- src/components/shared/LanguageSwitcher.tsx
+++ src/components/shared/LanguageSwitcher.tsx
@@ -1,25 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { cn } from '@/lib/utils';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
-import { useRouter } from 'next/router';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { cn } from "@/lib/utils";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { useRouter } from "next/router";
+import { useTranslation } from "react-i18next";
 
 interface LanguageSwitcherProps {
   className?: string;
 }
-
 export const LanguageSwitcher = ({ className }: LanguageSwitcherProps) => {
   const router = useRouter();
   const { i18n, t } = useTranslation();
-  
-  const [isLanguageLoaded, setIsLanguageLoaded] = useState(false);
 
-  useEffect(() => {
-    const storedLocale = localStorage.getItem('locale');
-    if (storedLocale && storedLocale !== i18n.language) {
-      i18n.changeLanguage(storedLocale);
+  const [currentLanguage, setCurrentLanguage] = React.useState<
+    string | undefined
+  >(undefined);
+
+  React.useEffect(() => {
+    const storedLocale = localStorage.getItem("locale");
+
+    const languageToUse = storedLocale || i18n.language;
+
+    if (languageToUse !== i18n.language) {
+      i18n.changeLanguage(languageToUse).then(() => {
+        setCurrentLanguage(languageToUse);
+      });
     } else {
-      setIsLanguageLoaded(true);
+      setCurrentLanguage(languageToUse);
     }
   }, [i18n]);
 
@@ -27,24 +39,24 @@ export const LanguageSwitcher = ({ className }: LanguageSwitcherProps) => {
     const { pathname, asPath, query } = router;
 
     router.push({ pathname, query }, asPath, { locale: newLocale }).then(() => {
-      localStorage.setItem('locale', newLocale);
+      localStorage.setItem("locale", newLocale);
       i18n.changeLanguage(newLocale);
+      setCurrentLanguage(newLocale);
     });
   };
 
-  if (!isLanguageLoaded) {
-    return null;
-  }
-
   return (
     <div className={cn(className)}>
-      <Select value={i18n.language} onValueChange={(value) => onToggleLanguageClick(value)}>
+      <Select
+        value={currentLanguage} // only set when defined
+        onValueChange={onToggleLanguageClick}
+      >
         <SelectTrigger>
-          <SelectValue />
+          <SelectValue placeholder={t("selectLanguage")} />
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value="fr">{t('languages.fr')}</SelectItem>
-          <SelectItem value="en">{t('languages.en')}</SelectItem>
+          <SelectItem value="fr">{t("languages.fr")}</SelectItem>
+          <SelectItem value="en">{t("languages.en")}</SelectItem>
         </SelectContent>
       </Select>
     </div>
