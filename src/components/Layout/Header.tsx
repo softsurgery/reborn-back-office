b0115151d926666cda75b142d9049513a5d35538
Houssem Eddine Chibouni <houssemeddine.chibouni@fsb.ucar.tn>
2025-08-18 08:45:43 +0100

    chore: remove unused hook calls

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -5,19 +5,12 @@ import { ModeToggle } from "../shared/ModeToggle";
 import { LanguageSwitcher } from "../shared/LanguageSwitcher";
 import { Commander } from "../shared/Commander";
 import { UserNav } from "./UserNav";
-import { useSession } from "next-auth/react";
-import { useEmailUser } from "@/hooks/content/User/useEmailUser";
-import { identifyUser } from "@/lib/users-management/utils/identify-user.util";
 
 interface HeaderProps {
   className?: string;
 }
 
 export const Header = ({ className }: HeaderProps) => {
-  const { data: userData } = useSession();
-  const { user } = useEmailUser(userData?.user.email);
-  const identity = React.useMemo(() => identifyUser(user), [user]);
-
   return (
     <header
       className={cn(
@@ -30,7 +23,6 @@ export const Header = ({ className }: HeaderProps) => {
       <div className="flex justify-center items-center gap-4 ml-auto">
         <LanguageSwitcher />
         <ModeToggle />
-
         <UserNav />
       </div>
     </header>
