79e1a464134989b4d44e0541a37bf74b1cd44080
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-07-14 14:59:54 +0100

    chore: lint spinner component

--- src/components/shared/Spinner.tsx
+++ src/components/shared/Spinner.tsx
@@ -1,43 +1,55 @@
-import React from 'react';
-import { cn } from '@/lib/utils';
-import { VariantProps, cva } from 'class-variance-authority';
-import { Loader2 } from 'lucide-react';
+import React from "react";
+import { cn } from "@/lib/utils";
+import { VariantProps, cva } from "class-variance-authority";
+import { Loader2 } from "lucide-react";
 
-const spinnerVariants = cva('flex-col items-center justify-center', {
+const spinnerVariants = cva("flex-col items-center justify-center", {
   variants: {
     show: {
-      true: 'flex',
-      false: 'hidden'
-    }
+      true: "flex",
+      false: "hidden",
+    },
   },
   defaultVariants: {
-    show: true
-  }
+    show: true,
+  },
 });
 
-const loaderVariants = cva('animate-spin text-primary', {
+const loaderVariants = cva("animate-spin text-primary", {
   variants: {
     size: {
-      small: 'size-6',
-      medium: 'size-8',
-      large: 'size-12'
-    }
+      small: "size-6",
+      medium: "size-8",
+      large: "size-12",
+    },
   },
   defaultVariants: {
-    size: 'medium'
-  }
+    size: "medium",
+  },
 });
 
 interface SpinnerContentProps
-  extends VariantProps<typeof spinnerVariants>,
+  extends
+    VariantProps<typeof spinnerVariants>,
     VariantProps<typeof loaderVariants> {
   className?: string;
   children?: React.ReactNode;
 }
 
-export function Spinner({ className, size, show, children }: SpinnerContentProps) {
+export function Spinner({
+  className,
+  size,
+  show,
+  children,
+}: SpinnerContentProps) {
   return (
-    <div className={cn('flex items-center justify-center', spinnerVariants({ show }), className)}>
+    <div
+      className={cn(
+        "flex items-center justify-center",
+        spinnerVariants({ show }),
+        className,
+      )}
+    >
       <span>
         <Loader2 className={cn(loaderVariants({ size }))} />
         {children}
